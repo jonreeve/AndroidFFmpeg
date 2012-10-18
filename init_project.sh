e0da25ba620cfb6ba89dff939f7f7d9ffb81a25f
Jon Reeve <themightyjon@gmail.com>
2012-10-18 22:59:06 +0100

    slightly better init script

--- init_project.sh
+++ init_project.sh
@@ -1,25 +1,37 @@
+#!/bin/bash
+
+set -e # bail on error
+
 git submodule init
 git submodule sync #if you are updating source code
-git submodule update
+GIT_SSL_NO_VERIFY=true git submodule update
+
 cd FFmpegLibrary/jni
 
-cd freetype
+echo freetype2
+cd freetype2
 ./autogen.sh
 cd ..
 
+echo fribidi
 cd fribidi
 autoreconf -ivf
 cd ..
 
+echo libass
 cd libass
 autoreconf -ivf
 cd ..
 
+echo vo-aacenc
 cd vo-aacenc
 autoreconf
 cd ..
 
+echo vo-amrwbenc
 cd vo-amrwbenc
 autoreconf
 cd ..
+
+echo Done!
 
